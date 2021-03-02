108ef61461806817d84a4b79065716d4f7d0cee4
Magva7 <magva7@gmail.com>
2021-03-03 00:19:03 +0300

    Все-таки разобрался с выводом функций

--- output_func.js
+++ output_func.js
@@ -1,29 +1,37 @@
-(function(window) {
+// (function(window) {
+//
+//     var obj = {};
+//
+//
+//     obj.dreamOn = function () {
+//     console.log("I want to see the global scope! Let me out!");
+//     };
+//
+//     // window.doer = obj;
+//     window.doer = obj;
+//
+// })('window');
+//
+// var c = {myname:"2", 3:"4"};
+// console.log(c.myname);
+// console.log(window.Date(time));
 
-    var obj = {};
+(function (window) {
 
-    obj.dreamOn = function () {
-    console.log("I want to see the global scope! Let me out!");
+    /*Объявляем объект b*/
+    var b = {};
+
+    /*У объекта b в свойство property1 записываем функцию, которая будет выводить сообщение в консоль*/
+    b.property1 = function (){
+        console.log("I want to see the global scope! Let me out!");
     };
 
-    // window.doer = obj;
-    window.doer = obj;
+    /*У объекта window в свойство с записываем сам объект b, чтобы потом к нему снаружи можно было обращаться, но
+     могли бы это свойство у window как угодно назвать, например window.b = b, и потом вызывать b.property1(), тоже
+      бы работало, т.е. когда мы пишем с.property1(), то вызываем в глобальной области видимости, т.е. как бы
+       пишем window.с.property1(), т.е. вызывается то, что у window записано в свойстве c и дальше в свойстве property1*/
+    window.с = b;
 
-})('window');
+})(window);
 
-var c = {myname:"2", 3:"4"};
-console.log(c.myname);
-console.log(window.Date(time));
-
-// (function (window) {
-//
-//     var b = {};
-//     b.output1 = function (){
-//         console.log("I want to see the global scope! Let me out!");
-//     };
-//     window.output2 = b;
-//     // window.output2 = b.output1;
-//     b.output1();
-// })();
-// // a();
-// // window.output2();
+с.property1();
